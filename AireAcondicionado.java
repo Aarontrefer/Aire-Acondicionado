09c613969458b7ac66faf9fd9ca8c204ba134922
Aaron Treceño <aarontreceno10@gmail.com>
2021-11-19 14:03:16 +0100

    0497.02 he empezado ha crear el aire acondicionado

--- AireAcondicionado.java
+++ AireAcondicionado.java
@@ -1,7 +1,20 @@
 public class AireAcondicionado{
+    private double temperatura;
     
-
-
+    public AireAcondicionado(){
+        temperatura = 15.0;
+    }
+    public double getTemperatura(){
+        return temperatura;
+    }
+    
+    public void subirTemperatura(){
+        temperatura += 5;
+     }
+     
+    public void bajarTemperatura(){
+        temperatura -= 5;  
+    }
 
 
 
