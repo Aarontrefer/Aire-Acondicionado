9201e54ffcd23687910ce4ed8f16373a56292f40
Aaron Treceño <aarontreceno10@gmail.com>
2021-11-22 14:01:02 +0100

    0497.04 he creado un metodo de estadisticas del aire acondicionado

--- AireAcondicionado.java
+++ AireAcondicionado.java
@@ -3,17 +3,23 @@ public class AireAcondicionado{
     private double min;
     private double max;
     private double incremento;
+    private int cambioTemperaturas;
+    private double maxTemperaturaAlcanzada;
+    private double minTemperaturaAlcanzada;
     public AireAcondicionado(double minTem, double maxTem){
         temperatura = 15.0;
         min = minTem;
         max = maxTem;
         incremento = 5.0;
+        cambioTemperaturas = 0;
+        maxTemperaturaAlcanzada = temperatura;
+        minTemperaturaAlcanzada = temperatura;
     }
     public double getTemperatura(){
         return temperatura;
     }
     
-    public void setIncre(double incre){
+    public void setIncremento(double incre){
         if (incre >= 0){
             incremento = incre;
         }
@@ -24,7 +30,10 @@ public class AireAcondicionado{
         if(maximo <= max){
             temperatura += incremento;
         }
-        
+        cambioTemperaturas += 1;
+        if(temperatura >= maxTemperaturaAlcanzada){
+            maxTemperaturaAlcanzada = temperatura;
+        }
      }
      
     public void bajarTemperatura(){
@@ -32,7 +41,19 @@ public class AireAcondicionado{
         if(minimo >= min){
             temperatura -= incremento;
         }
+        cambioTemperaturas += 1;
+        if(temperatura <= minTemperaturaAlcanzada){
+            minTemperaturaAlcanzada = temperatura;
+        }
+    }
     
+    public String getEstadisticas(){
+        String estadistica ="La temperatura actual es: " + temperatura + " - la temperatura maxima posible es: " + max + " - la temperatura minima posible es: " +  min  + " - la temperatura maxima alcanzada es: " + maxTemperaturaAlcanzada + " - la temperatura minima alcanzada es: " + minTemperaturaAlcanzada + " - la temperatura se ha cambiado estas veces: " + cambioTemperaturas;
+        return estadistica;
+    }
+    
+    public void mostrarEstadisticas(){
+         System.out.println("La temperatura actual es: " + temperatura + " - la temperatura maxima posible es: " + max + " - la temperatura minima posible es: " +  min  + " - la temperatura maxima alcanzada es: " + maxTemperaturaAlcanzada + " - la temperatura minima alcanzada es: " + minTemperaturaAlcanzada + " - la temperatura se ha cambiado estas veces: " + cambioTemperaturas);
     }
 
 
